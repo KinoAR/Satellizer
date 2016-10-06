51b443e25c80b7e776bbfaa24d472bd9e4a3a123
KinoAR <kinoarose@gmail.com>
2016-10-06 12:52:42 -0400

    Add necessary dependencies, improve code, add angular templates

---
+++ js/controller.js
@@ -0,0 +1,12 @@
+const SatellizerApp = angular.module("SatellizerApp", []);
+
+SatellizerApp.controller("TimerCtrl", ['$scope', function($scope) {
+  $scope.startTime = () => {
+
+  };
+
+  $scope.createTimeSheet = () => {
+
+  };
+
+}]);
